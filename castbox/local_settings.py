6269e40f23a4cd2d42471a286d7759c918612828
Mohammad Ali <forrest482@outlook.com>
2024-03-07 12:19:25 +0330

    ini

---
+++ castbox/local_settings.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3y=ih$r743)$5tovqom6vgsrgs-x=q2@0j-zzajv$25o9q25ez'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+ADMIN_PATH = 'admin/'
